ffcb7a16ac9eda4bbabe992e1efa1f5988d2d269
Bruno Novaes <15158330+bvnovaes@users.noreply.github.com>
2024-10-02 13:38:01 -0300

    Update DependencyInjection.cs

--- ErrorDetailsExample.Api/DependencyInjection.cs
+++ ErrorDetailsExample.Api/DependencyInjection.cs
@@ -6,6 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddProblemDetails();
         services.AddAutoMapper(typeof(MappingProfile));
         
         return services;
